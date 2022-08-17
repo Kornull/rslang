abd8ed745bee1c7a226ea120d122ce8030610f28
Kornull <wolod1987kor@gmail.com>
2022-08-18 02:41:52 +0300

    feat: add closed burger

--- src/components/Controller/burger/burger.ts
+++ src/components/Controller/burger/burger.ts
@@ -3,19 +3,22 @@ import './_burger.scss';
 const burger = <HTMLElement>document.querySelector('#burger');
 const shadowElement = <HTMLElement>document.querySelector('#shadow');
 const linkNavigation = document.querySelectorAll('.header__nav-link');
+const span = <HTMLElement>burger.lastChild;
+const headerMenu = <HTMLElement>document.querySelector('#header-menu');
+
+const closeBurger = () => {
+  span.classList.remove('active');
+  headerMenu.style.right = '-100%';
+  shadowElement.style.left = '-100%';
+  linkNavigation.forEach((link: Element) => {
+    link.setAttribute('tabindex', '-1');
+  });
+  document.body.classList.remove('active');
+};
 
 const clickBurger = (): void => {
-  const span = <HTMLElement>burger.lastChild;
-  const headerMenu = <HTMLElement>document.querySelector('#header-menu');
-
   if (span.classList.contains('active')) {
-    span.classList.remove('active');
-    headerMenu.style.right = '-100%';
-    shadowElement.style.left = '-100%';
-    linkNavigation.forEach((link: Element) => {
-      link.setAttribute('tabindex', '-1');
-    });
-    document.body.classList.remove('active');
+    closeBurger();
   } else {
     span.classList.add('active');
     headerMenu.style.right = '0%';
@@ -48,3 +51,5 @@ const clickBurger = (): void => {
     });
   });
 })();
+
+window.addEventListener('resize', closeBurger);
