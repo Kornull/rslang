0576c86d342669640b2f953082fe94e7f19a6b20
Svetlana-85 <svetlanka-85@yandex.ru>
2022-09-01 12:48:46 +0500

    feat: add new file cardWord.ts

---
+++ src/components/View/textbook/cardWord.ts
@@ -0,0 +1,127 @@
+import { getAggregateWordsUser, getListHardWord, getListWords } from '../../Controller/textbook/textbook';
+import { urlLink } from '../../Templates/serve';
+import { Word, WordAggregated } from '../../Types/word';
+import { createEl } from '../create_element';
+import { COUNT_GROUP, isAutorized, USER } from './textbook';
+import { updateHardWord, updateLearnWord } from './word';
+
+function createButtonAudio(wordValue: Word): SVGSVGElement {
+  const audioImg = <SVGSVGElement>document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  audioImg.classList.add('imgAudio');
+  audioImg.innerHTML = '<use xlink:href="./assets/img/audio.svg#audio"></use>';
+  audioImg.addEventListener('click', () => {
+    const audioObj = new Audio();
+    audioObj.src = `${urlLink}${wordValue.audio}`;
+    audioObj.autoplay = true;
+    audioObj.addEventListener('ended', () => {
+      const audioMeaning = new Audio();
+      audioMeaning.src = `${urlLink}${wordValue.audioMeaning}`;
+      audioMeaning.autoplay = true;
+      audioMeaning.addEventListener('ended', () => {
+        const audioExample = new Audio();
+        audioExample.src = `${urlLink}${wordValue.audioExample}`;
+        audioExample.autoplay = true;
+      });
+    });
+  });
+  return audioImg;
+}
+
+export async function renderCardWord(wordValue: Word, type: string): Promise<HTMLDivElement> {
+  const cardWord = <HTMLDivElement>createEl('div', undefined, ['cardWord', `group-${wordValue.group + 1}`], { id: `cardWord-${wordValue.id}` });
+  const multimedia = <HTMLDivElement>createEl('div', cardWord, ['multimediaBlock']);
+  const imgBlock = <HTMLDivElement>createEl('div', multimedia, ['imgBlock']);
+  createEl('img', imgBlock, ['imgWord'], { id: `imgWord-${wordValue.id}`, src: `${urlLink}${wordValue.image}` });
+  const buttonAdd = <HTMLDivElement>createEl('div', multimedia, ['buttonAdd']);
+  const audioImg: SVGSVGElement = createButtonAudio(wordValue);
+  buttonAdd.append(audioImg);
+  if (isAutorized()) {
+    const imgLearn = <SVGSVGElement>document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    imgLearn.classList.add('imgLearn');
+    imgLearn.innerHTML = '<use xlink:href="./assets/img/checkmark.svg#check"></use>';
+    buttonAdd.append(imgLearn);
+    imgLearn.addEventListener('click', () => updateLearnWord(wordValue, cardWord, USER));
+    const imgHard = <SVGSVGElement>document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    imgHard.classList.add('imgHard');
+    imgHard.innerHTML = '<use xlink:href="./assets/img/kettlebell.svg#kettlebell"></use>';
+    imgHard.addEventListener('click', () => updateHardWord(wordValue, cardWord, USER));
+    buttonAdd.append(imgHard);
+    if (type === 'learned') {
+      cardWord.classList.add('cardLearned');
+    }
+    if (type === 'hard') {
+      cardWord.classList.add('cardHard');
+    }
+  }
+  const word = <HTMLDivElement>createEl('div', cardWord, ['word']);
+  const wordLang = createEl('h3', word, ['h3']);
+  wordLang.innerHTML = `${wordValue.word} (${wordValue.wordTranslate})`;
+  const wordTranscript = createEl('h4', word, ['h4']);
+  wordTranscript.innerHTML = `${wordValue.transcription}`;
+  const textMeaning = createEl('p', word, ['p']);
+  textMeaning.innerHTML = `${wordValue.textMeaning}`;
+  const textMeaningTranslate = createEl('p', word, ['p']);
+  textMeaningTranslate.innerHTML = `${wordValue.textMeaningTranslate}`;
+  const textExample = createEl('p', word, ['p']);
+  textExample.innerHTML = `${wordValue.textExample}`;
+  const textExampleTranslate = createEl('p', word, ['p']);
+  textExampleTranslate.innerHTML = `${wordValue.textExampleTranslate}`;
+  return cardWord;
+}
+
+async function renderCardsNoAutorizedUser(currentGroup: string, currentPage: string, wrapperPageTextbook: HTMLDivElement): Promise<void> {
+  let cardWord: HTMLDivElement;
+  const listWords: Word[] = await getListWords(+currentGroup, +currentPage);
+  listWords.forEach(async (item) => {
+    cardWord = await renderCardWord(item, '');
+    wrapperPageTextbook.append(cardWord);
+  });
+}
+
+export async function renderCardsAutorizedUser(currentGroup: string, currentPage: string, wrapperPageTextbook: HTMLDivElement): Promise<void> {
+  let cardWord: HTMLDivElement;
+  let listWord: Array<WordAggregated>;
+  try {
+    if (Number(currentGroup) < COUNT_GROUP) listWord = await getAggregateWordsUser(USER, +currentGroup, +currentPage);
+    else listWord = await getListHardWord(USER);
+  } catch {
+    renderCardsNoAutorizedUser(currentGroup, currentPage, wrapperPageTextbook);
+    return;
+  }
+  listWord.forEach(async (item) => {
+    const word: Word = {
+      // eslint-disable-next-line no-underscore-dangle
+      id: item._id,
+      group: item.group,
+      page: item.page,
+      word: item.word,
+      image: item.image,
+      audio: item.audio,
+      audioMeaning: item.audioMeaning,
+      audioExample: item.audioExample,
+      textMeaning: item.textMeaning,
+      textExample: item.textExample,
+      transcription: item.transcription,
+      wordTranslate: item.wordTranslate,
+      textMeaningTranslate: item.textMeaningTranslate,
+      textExampleTranslate: item.textExampleTranslate,
+    };
+    let type = '';
+    if (item.userWord) {
+      if (item.userWord.difficulty === 'hard') type = 'hard';
+      else if (item.userWord.difficulty === 'easy' && item.userWord.optional?.statuslearn === 'true') {
+        type = 'learned';
+      }
+    }
+    cardWord = await renderCardWord(word, type);
+    if (item.userWord) {
+      cardWord.setAttribute('data-wordUser', 'true');
+      if (item.userWord.difficulty === 'hard') {
+        cardWord.setAttribute('data-WordHard', 'true');
+      } else if (item.userWord.difficulty === 'easy' && item.userWord.optional?.statuslearn === 'true') {
+        cardWord.setAttribute('data-WordLearned', 'true');
+      }
+    }
+    wrapperPageTextbook.append(cardWord);
+  });
+}
--- src/components/View/textbook/textbook.ts
+++ src/components/View/textbook/textbook.ts
@@ -1,140 +1,17 @@
-import * as controllerTextbook from '../../Controller/textbook/textbook';
-import { urlLink } from '../../Templates/serve';
-import { Word, WordAggregated } from '../../Types/word';
 import { createEl } from '../create_element';
 import { getLocalStorage, getStorage, setStorage } from '../../Controller/storage';
 import './_textbook.scss';
 import User from '../../Controller/authorization/user';
-import { updateHardWord, updateLearnWord } from './word';
+// eslint-disable-next-line import/no-cycle
+import { renderCardsAutorizedUser } from './cardWord';
 
-const COUNT_GROUP = 6;
+export const COUNT_GROUP = 6;
 const COUNT_PAGE_GROUP = 30;
-const USER: User = getLocalStorage('userDataBasic');
+export const USER: User = getLocalStorage('userDataBasic');
 
-function createButtonAudio(wordValue: Word): SVGSVGElement {
-  const audioImg = <SVGSVGElement>document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  audioImg.classList.add('imgAudio');
-  audioImg.innerHTML = '<use xlink:href="./assets/img/audio.svg#audio"></use>';
-  audioImg.addEventListener('click', () => {
-    const audioObj = new Audio();
-    audioObj.src = `${urlLink}${wordValue.audio}`;
-    audioObj.autoplay = true;
-    audioObj.addEventListener('ended', () => {
-      const audioMeaning = new Audio();
-      audioMeaning.src = `${urlLink}${wordValue.audioMeaning}`;
-      audioMeaning.autoplay = true;
-      audioMeaning.addEventListener('ended', () => {
-        const audioExample = new Audio();
-        audioExample.src = `${urlLink}${wordValue.audioExample}`;
-        audioExample.autoplay = true;
-      });
-    });
-  });
-  return audioImg;
-}
-
-function isAutorized() {
+export function isAutorized() {
   if (!USER.userId) return false;
   return true;
-}
-
-async function renderCardWord(wordValue: Word, type: string): Promise<HTMLDivElement> {
-  const cardWord = <HTMLDivElement>createEl('div', undefined, ['cardWord', `group-${wordValue.group + 1}`], { id: `cardWord-${wordValue.id}` });
-  const multimedia = <HTMLDivElement>createEl('div', cardWord, ['multimediaBlock']);
-  const imgBlock = <HTMLDivElement>createEl('div', multimedia, ['imgBlock']);
-  createEl('img', imgBlock, ['imgWord'], { id: `imgWord-${wordValue.id}`, src: `${urlLink}${wordValue.image}` });
-  const buttonAdd = <HTMLDivElement>createEl('div', multimedia, ['buttonAdd']);
-  const audioImg: SVGSVGElement = createButtonAudio(wordValue);
-  buttonAdd.append(audioImg);
-  if (isAutorized()) {
-    const imgLearn = <SVGSVGElement>document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    imgLearn.classList.add('imgLearn');
-    imgLearn.innerHTML = '<use xlink:href="./assets/img/checkmark.svg#check"></use>';
-    buttonAdd.append(imgLearn);
-    imgLearn.addEventListener('click', () => updateLearnWord(wordValue, cardWord, USER));
-    const imgHard = <SVGSVGElement>document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    imgHard.classList.add('imgHard');
-    imgHard.innerHTML = '<use xlink:href="./assets/img/kettlebell.svg#kettlebell"></use>';
-    imgHard.addEventListener('click', () => updateHardWord(wordValue, cardWord, USER));
-    buttonAdd.append(imgHard);
-    if (type === 'learned') {
-      cardWord.classList.add('cardLearned');
-    }
-    if (type === 'hard') {
-      cardWord.classList.add('cardHard');
-    }
-  }
-  const word = <HTMLDivElement>createEl('div', cardWord, ['word']);
-  const wordLang = createEl('h3', word, ['h3']);
-  wordLang.innerHTML = `${wordValue.word} (${wordValue.wordTranslate})`;
-  const wordTranscript = createEl('h4', word, ['h4']);
-  wordTranscript.innerHTML = `${wordValue.transcription}`;
-  const textMeaning = createEl('p', word, ['p']);
-  textMeaning.innerHTML = `${wordValue.textMeaning}`;
-  const textMeaningTranslate = createEl('p', word, ['p']);
-  textMeaningTranslate.innerHTML = `${wordValue.textMeaningTranslate}`;
-  const textExample = createEl('p', word, ['p']);
-  textExample.innerHTML = `${wordValue.textExample}`;
-  const textExampleTranslate = createEl('p', word, ['p']);
-  textExampleTranslate.innerHTML = `${wordValue.textExampleTranslate}`;
-  return cardWord;
-}
-
-async function renderCardsNoAutorizedUser(currentGroup: string, currentPage: string, wrapperPageTextbook: HTMLDivElement): Promise<void> {
-  let cardWord: HTMLDivElement;
-  const listWords: Word[] = await controllerTextbook.getListWords(+currentGroup, +currentPage);
-  listWords.forEach(async (item) => {
-    cardWord = await renderCardWord(item, '');
-    wrapperPageTextbook.append(cardWord);
-  });
-}
-
-async function renderCardsAutorizedUser(currentGroup: string, currentPage: string, wrapperPageTextbook: HTMLDivElement): Promise<void> {
-  let cardWord: HTMLDivElement;
-  let listWord: Array<WordAggregated>;
-  try {
-    if (Number(currentGroup) < COUNT_GROUP) listWord = await controllerTextbook.getAggregateWordsUser(USER, +currentGroup, +currentPage);
-    else listWord = await controllerTextbook.getListHardWord(USER);
-  } catch {
-    renderCardsNoAutorizedUser(currentGroup, currentPage, wrapperPageTextbook);
-    return;
-  }
-  listWord.forEach(async (item) => {
-    const word: Word = {
-      // eslint-disable-next-line no-underscore-dangle
-      id: item._id,
-      group: item.group,
-      page: item.page,
-      word: item.word,
-      image: item.image,
-      audio: item.audio,
-      audioMeaning: item.audioMeaning,
-      audioExample: item.audioExample,
-      textMeaning: item.textMeaning,
-      textExample: item.textExample,
-      transcription: item.transcription,
-      wordTranslate: item.wordTranslate,
-      textMeaningTranslate: item.textMeaningTranslate,
-      textExampleTranslate: item.textExampleTranslate,
-    };
-    let type = '';
-    if (item.userWord) {
-      if (item.userWord.difficulty === 'hard') type = 'hard';
-      else if (item.userWord.difficulty === 'easy' && item.userWord.optional?.statuslearn === 'true') {
-        type = 'learned';
-      }
-    }
-    cardWord = await renderCardWord(word, type);
-    if (item.userWord) {
-      cardWord.setAttribute('data-wordUser', 'true');
-      if (item.userWord.difficulty === 'hard') {
-        cardWord.setAttribute('data-WordHard', 'true');
-      } else if (item.userWord.difficulty === 'easy' && item.userWord.optional?.statuslearn === 'true') {
-        cardWord.setAttribute('data-WordLearned', 'true');
-      }
-    }
-    wrapperPageTextbook.append(cardWord);
-  });
 }
 
 export async function renderPageTextbook() {
@@ -145,7 +22,7 @@ export async function renderPageTextbook() {
   if (user.userId) {
     renderCardsAutorizedUser(currentGroup, currentPage, wrapperPageTextbook);
   } else {
-    renderCardsNoAutorizedUser(currentGroup, currentPage, wrapperPageTextbook);
+    renderCardsAutorizedUser(currentGroup, currentPage, wrapperPageTextbook);
   }
   return wrapperPageTextbook;
 }
