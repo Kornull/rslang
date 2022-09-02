e6715d1d6f0ea0681a2d722a41484896e3c9ebb6
Maxlablearn <maxlablearn@gmail.com>
2022-09-02 12:10:27 +0300

    feat: add game page markyp

--- src/components/View/audio-call-game/audio-call-game.ts
+++ src/components/View/audio-call-game/audio-call-game.ts
@@ -2,6 +2,11 @@ import './_audio-call.scss';
 import { main } from '../../Templates/main-block';
 import { createEl } from '../../Controller/createTagBlock';
 
+enum TextAudioGame {
+  gameHeader = 'Для старта игры выберите уровень сложности',
+  gameRules = '<span>Правила игры</span> очень просты: вам нужно выбрать правильный перевод прослушанного слова',
+}
+
 function createButtons(parentElement: HTMLElement, count: number): void {
   const buttonsField = createEl('div', parentElement, ['btn-field']);
   for (let i = 1; i < count + 1; i++) {
@@ -17,5 +22,35 @@ function createButtons(parentElement: HTMLElement, count: number): void {
 
 export function createAudioGame() {
   main.innerHTML = '';
-  createButtons(main, 5);
+  const gameField = <HTMLElement>createEl('div', main, ['audio-game']);
+  const viewField = <HTMLElement>createEl('div', gameField, ['view']);
+  const imageContainer = <HTMLElement>createEl('div', viewField, ['image-container']);
+  createEl('img', imageContainer);
+  const audioField = <HTMLElement>createEl('div', viewField, ['audio-field']);
+  const audioButton = <HTMLElement>createEl('button', audioField, ['audio-button']);
+  audioButton.innerHTML = '<svg class="audio-svg-icon" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path></svg>';
+  const engWord = <HTMLElement>createEl('p', audioField, ['english-word']);
+  engWord.innerText = 'computer';
+  createButtons(gameField, 5);
+  const nextSkipBtn = createEl('button', gameField, ['next-skip-btn']);
+  nextSkipBtn.innerText = 'Следующий';
 }
+
+export function createAudoGamePreload() {
+  main.innerHTML = '';
+  const preloadAudioPage = <HTMLElement>createEl('div', main, ['audio-preload']);
+  const preloadMessage = <HTMLElement>createEl('p', preloadAudioPage, ['preload-message']);
+  preloadMessage.innerText = 
+}
+
+// <svg class="CircularProgressbar " viewBox="0 0 100 100" data-test-id="CircularProgressbar"><path class="CircularProgressbar-trail" d="
+//       M 50,50
+//       m 0,-46
+//       a 46,46 0 1 1 0,92
+//       a 46,46 0 1 1 0,-92
+//     " stroke-width="8" fill-opacity="0" style="stroke: rgb(255, 255, 255); stroke-dasharray: 289.027px, 289.027px; stroke-dashoffset: 0px;"></path><path class="CircularProgressbar-path" d="
+//       M 50,50
+//       m 0,-46
+//       a 46,46 0 1 1 0,92
+//       a 46,46 0 1 1 0,-92
+//     " stroke-width="8" fill-opacity="0" style="stroke: rgb(242, 105, 92); stroke-dasharray: 289.027px, 289.027px; stroke-dashoffset: 130.062px;"></path><text class="CircularProgressbar-text" x="50" y="50" style="fill: rgb(242, 105, 92);">11/20</text></svg>
