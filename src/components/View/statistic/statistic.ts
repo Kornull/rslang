bdca003d4e67397ede64eb0c249e1cc90f51f2c2
Svetlana-85 <svetlanka-85@yandex.ru>
2022-09-03 21:42:03 +0500

    feat: change title statistics

--- src/components/View/statistic/statistic.ts
+++ src/components/View/statistic/statistic.ts
@@ -7,8 +7,8 @@ import { StatisticsUserWords } from '../../Types/types';
 import './_statistic.scss';
 
 const titleStatistic = ['Аудиовызов', 'Спринт', 'Статистика по словам'];
-const rowStatistic = ['Количество новых слов', 'Процент правильных ответов', 'Лучшая серия'];
-const rowStatisticWords = ['Количество новых слов', 'Количество изученных слов', 'Процент правильных ответов'];
+const rowStatistic = ['Количество новых слов за день', 'Процент правильных ответов', 'Лучшая серия'];
+const rowStatisticWords = ['Количество новых слов за день', 'Количество изученных слов за день', 'Процент правильных ответов за день'];
 
 function createStatisticItem(title: string, rowTitle: Array<string>, rowValue: Array<number>): HTMLElement {
   const blockStat: HTMLElement = createEl('div', undefined, ['blockStat']);
@@ -31,26 +31,34 @@ export async function createStatisticPage() {
   title.innerText = 'Статистика';
   const statistic: HTMLElement = createEl('div', main, ['statistics']);
   const user: User = getLocalStorage('userDataBasic');
-  const stat: StatisticsUserWords = await getStatisticWords(user);
-  const audioResult = [
-    stat.optional.audioDayGuess ? stat.optional.audioDayGuess : 0,
-    stat.optional.audioAllDayWords && stat.optional.audioDayGuess
-      ? Math.round((stat.optional.audioDayGuess / stat.optional.audioAllDayWords) * 100)
-      : 0,
-    stat.optional.audioMaxGuessed ? stat.optional.audioMaxGuessed : 0,
-    stat.optional.audioAllDayWords ? stat.optional.audioAllDayWords : 0,
-  ];
-  const sprintResult = [
-    stat.optional.sprintDayGuess ? stat.optional.sprintDayGuess : 0,
-    Math.round((stat.optional.sprintDayGuess / stat.optional.sprintAllDayWords) * 100),
-    stat.optional.sprintMaxGuessed ? stat.optional.sprintMaxGuessed : 0,
-    stat.optional.sprintAllDayWords ? stat.optional.sprintAllDayWords : 0,
-  ];
-  const audioStat: HTMLElement = createStatisticItem(titleStatistic[0], rowStatistic, audioResult);
-  statistic.append(audioStat);
-  const sprintStat: HTMLElement = createStatisticItem(titleStatistic[1], rowStatistic, sprintResult);
-  statistic.append(sprintStat);
-  const percentTrue = Math.round(((audioResult[0] + sprintResult[0]) / (audioResult[3] + sprintResult[3])) * 100);
-  const wordsStat: HTMLElement = createStatisticItem(titleStatistic[2], rowStatisticWords, [audioResult[0] + sprintResult[0], 0, percentTrue]);
-  statistic.append(wordsStat);
+  try {
+    const stat: StatisticsUserWords = await getStatisticWords(user);
+    const audioResult = [
+      stat.optional.audioDayGuess ? stat.optional.audioDayGuess : 0,
+      stat.optional.audioAllDayWords && stat.optional.audioDayGuess
+        ? Math.round((stat.optional.audioDayGuess / stat.optional.audioAllDayWords) * 100)
+        : 0,
+      stat.optional.audioMaxGuessed ? stat.optional.audioMaxGuessed : 0,
+      stat.optional.audioAllDayWords ? stat.optional.audioAllDayWords : 0,
+    ];
+    const sprintResult = [
+      stat.optional.sprintDayGuess ? stat.optional.sprintDayGuess : 0,
+      Math.round((stat.optional.sprintDayGuess / stat.optional.sprintAllDayWords) * 100),
+      stat.optional.sprintMaxGuessed ? stat.optional.sprintMaxGuessed : 0,
+      stat.optional.sprintAllDayWords ? stat.optional.sprintAllDayWords : 0,
+    ];
+    const audioStat: HTMLElement = createStatisticItem(titleStatistic[0], rowStatistic, audioResult);
+    statistic.append(audioStat);
+    const sprintStat: HTMLElement = createStatisticItem(titleStatistic[1], rowStatistic, sprintResult);
+    statistic.append(sprintStat);
+    const percentTrue = Math.round(((audioResult[0] + sprintResult[0]) / (audioResult[3] + sprintResult[3])) * 100);
+    const wordsStat: HTMLElement = createStatisticItem(titleStatistic[2], rowStatisticWords, [
+      audioResult[0] + sprintResult[0],
+      stat.learnedWords ? stat.learnedWords : 0,
+      percentTrue,
+    ]);
+    statistic.append(wordsStat);
+  } catch {
+    statistic.innerHTML = 'Статистика доступна только авторизованному пользователю';
+  }
 }
