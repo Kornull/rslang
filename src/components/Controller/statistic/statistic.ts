8179ac213d16524fedb2a9b053b00cc081cbdb4d
Svetlana-85 <svetlanka-85@yandex.ru>
2022-09-04 17:21:01 +0500

    refactor: add types variables

--- src/components/Controller/statistic/statistic.ts
+++ src/components/Controller/statistic/statistic.ts
@@ -3,7 +3,7 @@ import { StatisticsUserWords } from '../../Types/types';
 import User from '../authorization/user';
 
 export async function getStatisticWords(user: User): Promise<StatisticsUserWords> {
-  const response = await fetch(`${urlLink}users/${user.userId}/statistics`, {
+  const response: Response = await fetch(`${urlLink}users/${user.userId}/statistics`, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${user.token}`,
@@ -17,9 +17,9 @@ export async function getStatisticWords(user: User): Promise<StatisticsUserWords
   return res;
 }
 
-export async function getStatisticLernWords(user: User) {
+export async function getStatisticLernWords(user: User): Promise<number> {
   const dataLearn = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`;
-  const response = await fetch(
+  const response: Response = await fetch(
     `${urlLink}users/${user.userId}/aggregatedWords?filter={"$and":[{"userWord.difficulty":"easy", "userWord.optional.statusLearn": "true", "userWord.optional.dataLearn": "${dataLearn}"}]}&wordsPerPage=3600`,
     {
       method: 'GET',
@@ -32,5 +32,6 @@ export async function getStatisticLernWords(user: User) {
   if (response.status === 404) {
     console.log('Error');
   }
-  return (await response.json())[0];
+  const result = (await response.json())[0].totalCount[0].count;
+  return result;
 }
--- src/components/View/statistic/statistic.ts
+++ src/components/View/statistic/statistic.ts
@@ -35,8 +35,8 @@ export async function createStatisticPage() {
   if (user.userId) {
     try {
       const stat: StatisticsUserWords = await getStatisticWords(user);
-      const countLernWords: number = (await getStatisticLernWords(user)).totalCount[0].count;
-      const audioResult = [
+      const countLernWords: number = await getStatisticLernWords(user);
+      const audioResult: number[] = [
         stat.optional.audiotDayGuess ? stat.optional.audiotDayGuess : 0,
         stat.optional.audioAllDayWords && stat.optional.audiotDayGuess
           ? Math.round((stat.optional.audiotDayGuess / stat.optional.audioAllDayWords) * 100)
@@ -44,7 +44,7 @@ export async function createStatisticPage() {
         stat.optional.audioMaxGuessed ? stat.optional.audioMaxGuessed : 0,
         stat.optional.audioAllDayWords ? stat.optional.audioAllDayWords : 0,
       ];
-      const sprintResult = [
+      const sprintResult: number[] = [
         stat.optional.sprintDayGuess ? stat.optional.sprintDayGuess : 0,
         stat.optional.sprintDayGuess && stat.optional.sprintAllDayWords
           ? Math.round((stat.optional.sprintDayGuess / stat.optional.sprintAllDayWords) * 100)
@@ -56,7 +56,7 @@ export async function createStatisticPage() {
       statistic.append(audioStat);
       const sprintStat: HTMLElement = createStatisticItem(titleStatistic[1], rowStatistic, sprintResult);
       statistic.append(sprintStat);
-      const percentTrue = Math.round(((audioResult[0] + sprintResult[0]) / (audioResult[3] + sprintResult[3])) * 100);
+      const percentTrue: number = Math.round(((audioResult[0] + sprintResult[0]) / (audioResult[3] + sprintResult[3])) * 100);
       const wordsStat: HTMLElement = createStatisticItem(titleStatistic[2], rowStatisticWords, [
         audioResult[0] + sprintResult[0],
         (stat.learnedWords ? stat.learnedWords : 0) + countLernWords,
